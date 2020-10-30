91cd329f039d107b9b9c039e14f9829f975ef59e
Jazmin CamG <jazmin.camacho7450@alumnos.udg.mx>
2020-10-30 01:36:59 -0600

    Agregar conexion y main de api

---
+++ conexion.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+bd = mysql.connector.connect(
+    user='jazmin', password='123456',
+    database='cinemapp')
+
+cursor = bd.cursor()
+
+def get_usuarios():
+    consulta = "SELECT * FROM usuario"
+
+    cursor.execute(consulta)
+    usuarios = []
+    for row in cursor.fetchall():
+        usuario = {
+            'id': row[0],
+            'correo': row[1],
+            'contraseña': row[2]
+        }
+        usuarios.append(usuario)
+    return usuarios
